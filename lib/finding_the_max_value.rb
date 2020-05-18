ceb4d8f39d618c28dd06380e35b52384a05119b5
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-18 05:07:08 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,9 @@
 def find_max_value(array)
-  # Add your solution here
+  maxval = 0
+  array.length.times do |i|
+    if array[i] > maxval 
+      maxval = array[i]
+    end
+  end
+  return maxval
 end
